4cdb8207622573eb9ec3ecf079ad516551156a1d
ankitbavadiya <ankitbavadiya1011@gmail.com>
2021-09-06 18:40:13 +0530

    Add news API and upgrade intent

--- actions/actions.py
+++ actions/actions.py
@@ -7,10 +7,11 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import requests
 #
 #
 # class ActionHelloWorld(Action):
@@ -25,3 +26,119 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+def newsapi(country):
+    """api call for getting top news hedline based on country """
+    urlcoun='https://newsapi.org/v2/top-headlines?country='+ country +'&apiKey=0594f5154edd4117b416e3db461b2f0d'
+    print(urlcoun)
+    response=requests.get(urlcoun)
+    data=response.json()
+    return data
+
+class newsHeadlineus(Action):
+    """example of custom action"""
+    def name(self):
+        """name of the custom action"""
+        return "action_news_headline_us"
+
+    def run(self,dispatcher,tracker,domain):
+        """displaying news headlines of us"""  
+        data=newsapi("us")
+        leng=len(data)
+        for i in range(leng):	
+            gt = {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [
+                                                    {
+                                "title": data['articles'][i]['title'],
+                                "image_url":data['articles'][i]['urlToImage'],
+                                "subtitle": data['articles'][i]['description'],
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": data['articles'][i]['url'],
+                                        "title": "Read More"
+                                    },
+                                ]
+                            },
+                        ]
+                    }
+                }
+            }  
+        dispatcher.utter_custom_json(gt)
+        return []
+
+class NewsheadlineIndia(Action):
+    """example of custom action"""
+    def name(self):
+        """name of the custom action"""
+        return "action_news_headline_india"
+
+    def run(self,dispatcher,tracker,domain):
+        """displaying news headlines of india"""  
+        data=newsapi("in")
+        leng=len(data)
+        for i in range(leng):	
+            gt = {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [
+                            {
+                                "title": data['articles'][i]['title'],
+                                "image_url":data['articles'][i]['urlToImage'],
+                                "subtitle": data['articles'][i]['description'],
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": data['articles'][i]['url'],
+                                        "title": "Read More"
+                                    },
+                                ]
+                            },
+                        ]
+                    }
+                }
+            } 
+            print(gt)
+            dispatcher.utter_message(json_message=gt)
+        return []
+
+class NewsHeadlineAustralia(Action):
+    """example of custom action"""  
+    def name(self):
+        """name of the custom action australia"""
+        return "action_news_headline_au"
+
+    def run(self,dispatcher,tracker,domain):
+        """displaying news headlines of """  
+        data=newsapi("au")
+        leng=len(data)
+        for i in range(leng):	
+            gt = {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [
+                                                    {
+                                "title": data['articles'][i]['title'],
+                                "image_url":data['articles'][i]['urlToImage'],
+                                "subtitle": data['articles'][i]['description'],
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": data['articles'][i]['url'],
+                                        "title": "Read More"
+                                    },
+                                ]
+                            },
+                        ]
+                    }
+                }
+            } 
+        dispatcher.utter_custom_json(gt)
+        return []
