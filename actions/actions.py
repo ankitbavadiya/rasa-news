d762c861dcf0e1d6afef873f0ba84b63cae49084
ankitbavadiya <ankitbavadiya1011@gmail.com>
2021-09-06 23:29:26 +0530

    Fix actions and stories

--- actions/actions.py
+++ actions/actions.py
@@ -1,17 +1,10 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import requests
+
+
 #
 #
 # class ActionHelloWorld(Action):
@@ -28,59 +21,25 @@ import requests
 #         return []
 def newsapi(country):
     """api call for getting top news hedline based on country """
-    urlcoun='https://newsapi.org/v2/top-headlines?country='+ country +'&apiKey=0594f5154edd4117b416e3db461b2f0d'
+    urlcoun = 'https://newsapi.org/v2/top-headlines?country=' + country + '&apiKey=0594f5154edd4117b416e3db461b2f0d'
     print(urlcoun)
-    response=requests.get(urlcoun)
-    data=response.json()
+    response = requests.get(urlcoun)
+    data = response.json()
     return data
+
 
 class newsHeadlineus(Action):
     """example of custom action"""
+
     def name(self):
         """name of the custom action"""
         return "action_news_headline_us"
 
-    def run(self,dispatcher,tracker,domain):
-        """displaying news headlines of us"""  
-        data=newsapi("us")
-        leng=len(data)
-        for i in range(leng):	
-            gt = {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "generic",
-                        "elements": [
-                                                    {
-                                "title": data['articles'][i]['title'],
-                                "image_url":data['articles'][i]['urlToImage'],
-                                "subtitle": data['articles'][i]['description'],
-                                "buttons": [
-                                    {
-                                        "type": "web_url",
-                                        "url": data['articles'][i]['url'],
-                                        "title": "Read More"
-                                    },
-                                ]
-                            },
-                        ]
-                    }
-                }
-            }  
-        dispatcher.utter_custom_json(gt)
-        return []
-
-class NewsheadlineIndia(Action):
-    """example of custom action"""
-    def name(self):
-        """name of the custom action"""
-        return "action_news_headline_india"
-
-    def run(self,dispatcher,tracker,domain):
-        """displaying news headlines of india"""  
-        data=newsapi("in")
-        leng=len(data)
-        for i in range(leng):	
+    def run(self, dispatcher, tracker, domain):
+        """displaying news headlines of us"""
+        data = newsapi("us")
+        leng = len(data)
+        for i in range(leng):
             gt = {
                 "attachment": {
                     "type": "template",
@@ -89,7 +48,7 @@ class NewsheadlineIndia(Action):
                         "elements": [
                             {
                                 "title": data['articles'][i]['title'],
-                                "image_url":data['articles'][i]['urlToImage'],
+                                "image_url": data['articles'][i]['urlToImage'],
                                 "subtitle": data['articles'][i]['description'],
                                 "buttons": [
                                     {
@@ -102,31 +61,32 @@ class NewsheadlineIndia(Action):
                         ]
                     }
                 }
-            } 
-            print(gt)
-            dispatcher.utter_message(json_message=gt)
+            }
+            dispatcher.utter_custom_json(gt)
         return []
 
-class NewsHeadlineAustralia(Action):
-    """example of custom action"""  
-    def name(self):
-        """name of the custom action australia"""
-        return "action_news_headline_au"
 
-    def run(self,dispatcher,tracker,domain):
-        """displaying news headlines of """  
-        data=newsapi("au")
-        leng=len(data)
-        for i in range(leng):	
+class NewsheadlineIndia(Action):
+    """example of custom action"""
+
+    def name(self):
+        """name of the custom action"""
+        return "action_news_headline_india"
+
+    def run(self, dispatcher, tracker, domain):
+        """displaying news headlines of india"""
+        data = newsapi("india")
+        leng = len(data)
+        for i in range(leng):
             gt = {
                 "attachment": {
                     "type": "template",
                     "payload": {
                         "template_type": "generic",
                         "elements": [
-                                                    {
+                            {
                                 "title": data['articles'][i]['title'],
-                                "image_url":data['articles'][i]['urlToImage'],
+                                "image_url": data['articles'][i]['urlToImage'],
                                 "subtitle": data['articles'][i]['description'],
                                 "buttons": [
                                     {
@@ -139,6 +99,45 @@ class NewsHeadlineAustralia(Action):
                         ]
                     }
                 }
-            } 
-        dispatcher.utter_custom_json(gt)
+            }
+            print(gt)
+            dispatcher.utter_message(response=gt)
+        return []
+
+
+class NewsHeadlineAustralia(Action):
+    """example of custom action"""
+
+    def name(self):
+        """name of the custom action australia"""
+        return "action_news_headline_au"
+
+    def run(self, dispatcher, tracker, domain):
+        """displaying news headlines of """
+        data = newsapi("au")
+        leng = len(data)
+        for i in range(leng):
+            gt = {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [
+                            {
+                                "title": data['articles'][i]['title'],
+                                "image_url": data['articles'][i]['urlToImage'],
+                                "subtitle": data['articles'][i]['description'],
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": data['articles'][i]['url'],
+                                        "title": "Read More"
+                                    },
+                                ]
+                            },
+                        ]
+                    }
+                }
+            }
+            dispatcher.utter_custom_json(gt)
         return []
